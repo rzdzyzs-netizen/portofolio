4d561f8764303cccb1e4a05528488725225184d7
rzdzyzs-netizen <rzdzyzs@gmail.com>
2026-03-10 11:52:09 +0700

    remove cursor, slide layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,16 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
-import CustomCursor from '@/components/CustomCursor'
 
 export default function Home() {
   return (
     <main className="relative">
-      {/* Scan line effect */}
       <div className="scan-line" />
-
-      {/* Custom cursor */}
-      <CustomCursor />
-
-      {/* Nav */}
       <Navbar />
-
-      {/* Sections */}
       <Hero />
       <About />
       <Projects />
-      <Contact/>
+      <Contact />
     </main>
   )
 }
